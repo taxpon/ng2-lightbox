5cbea2d8c1ed1cb2d913e1c0db75d5422e6f27ce
Takuro Wada <taxpon@gmail.com>
2016-07-11 09:07:50 +0900

    Initial commit

---
+++ demo/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+  selector: 'app',
+  pipes: [],
+  providers: [],
+  template: '<div>This is the application</div>'
+})
+export class App {
+  constructor() {}
+
+}
